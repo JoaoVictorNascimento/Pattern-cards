267ba8dc6a1da18c758a232501ff36dc9e0a3698
JoaoVictorNascimento <joao_victorxy@hotmail.com>
2025-06-28 17:05:31 -0300

    feat: create abstract-factory implementation, adding carousel and card shadcn, clean default home page, and adding prettier config

---
+++ pattern-cards/app/components/PatternCard/card.tsx
@@ -0,0 +1,13 @@
+import { PatternCardProps } from "./types";
+
+export const PatternCard = (props: PatternCardProps) => {
+const { id, name, description } = props;
+  return (
+    <div>
+      <h1>Pattern Card</h1>
+      <span>{id}</span>
+      <span>{name}</span>
+      <span>{description}</span>
+    </div>
+  );
+};
